9b90bb1d21048e6bac386f4539d91d070462d0f3
Tom Wardill <tom@howrandom.net>
2015-01-13 11:59:32 +0000

    Preparing release 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stackhelper",
-    version='0.2.dev0',
+    version='0.2',
     packages=["stackhelper"],
     include_package_data=True,
     license="Apache Software License",
