7c4bb49c1e498dde1b76960de0d8ce2fabc05afb
John Carr <john.carr@isotoma.com>
2015-04-07 15:41:52 +0100

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stackhelper",
-    version='0.3',
+    version='0.4.dev0',
     packages=["stackhelper"],
     include_package_data=True,
     license="Apache Software License",
